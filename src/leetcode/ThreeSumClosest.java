f74b8698197573a85cac35d52e3342fa0cd50b9c
shashaag <shashank.agarwal@oracle.com>
2021-07-27 18:53:20 +0530

    ThreeSumClosest Two pointer solution O(n^2)

--- src/leetcode/ThreeSumClosest.java
+++ src/leetcode/ThreeSumClosest.java
@@ -1,18 +1,29 @@
 package leetcode;
 
+import java.util.Arrays;
+
 //https://leetcode.com/problems/3sum-closest/
 public class ThreeSumClosest {
     public int threeSumClosest(int[] nums, int target) {
         int diff = Integer.MAX_VALUE;
         int ans = -1;
+        Arrays.sort(nums);
         for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    int sum = nums[i] + nums[j] + nums[k];
-                    if (diff > Math.abs(sum - target)) {
-                        ans = sum;
-                        diff = Math.abs(sum - target);
-                    }
+            int sum;
+            int low = i+1, high = nums.length-1;
+            while(low<high) {
+                sum =  nums[i]+ nums[low] + nums[high];
+                if (diff > Math.abs(sum - target)) {
+                    ans = sum;
+                    diff = Math.abs(sum - target);
+                }
+                if (sum > target) {
+                    //we need to decrease the sum
+                    high--;
+                } else if (sum < target) {
+                    low++;
+                } else {
+                    return target;
                 }
             }
         }
@@ -22,6 +33,7 @@ public class ThreeSumClosest {
     public static void main(String[] args) {
         ThreeSumClosest obj = new ThreeSumClosest();
         System.out.println(obj.threeSumClosest(new int[]{-1, 2, 1, -4}, 1));
+        System.out.println(obj.threeSumClosest(new int[]{1,2,5,10,11}, 12));
         System.out.println(obj.threeSumClosest(new int[]{-1, 2, 1, -4}, -100));
     }
 
