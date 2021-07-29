191e5e9d492710ef02f557e709dee8a60210123c
shashaag <shashank.agarwal@oracle.com>
2021-07-29 21:15:58 +0530

    DP solution

--- src/leetcode/UpdateMatrix.java
+++ src/leetcode/UpdateMatrix.java
@@ -1,73 +1,44 @@
 package leetcode;
 
 //https://leetcode.com/problems/01-matrix/
+//https://leetcode.com/problems/01-matrix/solution/
+//BFS : https://leetcode.com/problems/01-matrix/discuss/1327052/Single-Reverse-BFS-approach-JAVA-Best-Clean-Code
+//DP : below solution
 public class UpdateMatrix {
 
-    private int[][] ans;
-
-    private boolean rightExists(int i, int  j) {
-        return j+1 < ans[0].length;
-    }
-
-    private boolean leftExists(int i, int  j) {
-        return j-1 >= 0;
-    }
-
-    private boolean upExists(int i, int  j) {
-        return i-1 >= 0;
-    }
-    private boolean downExists(int i, int  j) {
-        return i+1 < ans.length;
-    }
-
-    public boolean updateSurroundings(int[][] mat, int i, int j) {
-        if (ans[i][j] == ans.length*ans[0].length) {
-            //not eligible for updating surroundings
-            return false;
-        }
-        boolean updated = false;
-        if (rightExists(i, j) && ans[i][j+1] > ans[i][j]+1) {
-            updated = true;
-            ans[i][j+1] = ans[i][j]+1;
-        }
-        if (leftExists(i, j) && ans[i][j-1] > ans[i][j]+1) {
-            updated = true;
-            ans[i][j-1] = ans[i][j]+1;
-        }
-        if (upExists(i, j) && ans[i-1][j] > ans[i][j]+1) {
-            updated = true;
-            ans[i-1][j] = ans[i][j]+1;
-        }
-        if (downExists(i, j) && ans[i+1][j] > ans[i][j]+1) {
-            updated = true;
-            ans[i+1][j] = ans[i][j]+1;
-        }
-        return updated;
-    }
-
-    public int[][] updateMatrix(int[][] mat) {
-        int rows = mat.length, cols = mat[0].length;
-        ans = new int[rows][cols];
-        for (int i=0;i<rows;i++){
-            for (int j=0;j<cols;j++){
-                ans[i][j] = (mat[i][j] == 0? 0: rows*cols);
-            }
+    public int[][] updateMatrix(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return matrix;
         }
 
-        boolean updated = true;
-        int currentDistance = 0;
-        while(updated) {
-            updated = false;
-            for (int i=0;i<rows;i++){
-                for (int j=0;j<cols;j++){
-                    if (ans[i][j] == currentDistance) {
-                        updated = updateSurroundings(mat, i, j) || updated;
-                    }
+        int[][] dis = new int[matrix.length][matrix[0].length];
+        int range = matrix.length * matrix[0].length;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    dis[i][j] = 0;
+                } else {
+                    int upCell = (i > 0) ? dis[i - 1][j] : range;
+                    int leftCell = (j > 0) ? dis[i][j - 1] : range;
+                    dis[i][j] = Math.min(upCell, leftCell) + 1;
                 }
             }
-            currentDistance++;
         }
-        return ans;
+
+        for (int i = matrix.length - 1; i >= 0; i--) {
+            for (int j = matrix[0].length - 1; j >= 0; j--) {
+                if (matrix[i][j] == 0) {
+                    dis[i][j] = 0;
+                } else {
+                    int downCell = (i < matrix.length - 1) ? dis[i + 1][j] : range;
+                    int rightCell = (j < matrix[0].length - 1) ? dis[i][j + 1] : range;
+                    dis[i][j] = Math.min(Math.min(downCell, rightCell) + 1, dis[i][j]);
+                }
+            }
+        }
+
+        return dis;
     }
 
     public static void main(String[] args) {
