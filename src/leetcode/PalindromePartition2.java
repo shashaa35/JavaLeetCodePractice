168ed8db5304be92c6159df04b6caac3c2ad29fa
shashaag <shashank.agarwal@oracle.com>
2021-08-07 21:06:52 +0530

    Palindrom partition dp solution for loop with gap

--- src/leetcode/PalindromePartition2.java
+++ src/leetcode/PalindromePartition2.java
@@ -1,6 +1,6 @@
 package leetcode;
 
-//https://www.youtube.com/watch?v=lDYIvtBVmgo
+//https://www.youtube.com/watch?v=qmTtAbOTqcg
 //https://leetcode.com/problems/palindrome-partitioning-ii/
 public class PalindromePartition2 {
 
@@ -21,37 +21,37 @@ public class PalindromePartition2 {
         int[][] C = new int[n][n];
         boolean[][] P = new boolean[n][n];
 
-        int i, j, k, L; // different looping variables
-        for (L = 1; L <= n; L++) {
-            // For substring of length L, set different
-            // possible starting indexes
-            for (i = 0; i < n - L + 1; i++) {
-                j = i + L - 1; // Set ending index
-                if (L==1) {
+        int i, j, k, gap; // different looping variables
+        for (gap = 0; gap < n; gap++) {
+            for (i = 0, j = gap; j < n; i++, j++) {
+                if (gap == 0) {
                     P[i][j] = true;
-                } else if (L == 2)
+                } else if (gap == 1)
                     P[i][j] = (str.charAt(i) == str.charAt(j));
                 else
                     P[i][j] = (str.charAt(i) == str.charAt(j)) && P[i + 1][j - 1];
-
-                // IF str[i..j] is palindrome, then
-                // C[i][j] is 0
-                if (P[i][j])
-                    C[i][j] = 0;
-                else {
-                    // Make a cut at every possible
-                    // localtion starting from i to j,
-                    // and get the minimum cost cut.
-                    C[i][j] = Integer.MAX_VALUE;
-                    for (k = i; k <= j - 1; k++)
-                        C[i][j] = Integer.min(C[i][j],
-                                C[i][k] + C[k + 1][j] + 1);
-                }
             }
         }
 
-        // Return the min cut value for complete
-        // string. i.e., str[0..n-1]
+        for (gap = 0; gap < n; gap++) {
+            for (i = 0, j = gap; j < n; i++, j++) {
+                if (P[i][j]) {
+                    C[i][j] = 0;
+                } else {
+                    int min = Integer.MAX_VALUE;
+                    for (k = i;k<j;k++) {
+                        int left = C[i][k];
+                        int right = C[k+1][j];
+                        if (left + right + 1 < min) {
+                            min = left + right + 1;
+                        }
+                    }
+                    C[i][j] = min;
+                }
+
+            }
+        }
+
         return C[0][n - 1];
     }
 
